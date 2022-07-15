16bde485920863bfe6f994b6a661d5f47afeb5ee
Rapuška <megarubka44@gmail.com>
2022-07-15 22:25:56 +0300

    Refactored LevelRotator.cs

--- GameJame2022/Assets/CodeBase/Level/LevelRotator.cs
+++ GameJame2022/Assets/CodeBase/Level/LevelRotator.cs
@@ -3,25 +3,20 @@ using Vector3 = UnityEngine.Vector3;
 
 namespace CodeBase.Level
 {
-	[RequireComponent(typeof(Collider2D))]
 	public class LevelRotator : MonoBehaviour
 	{
 		private Camera _camera;
 		private Vector3 _screenPosition;
 		private float _angleOffset;
-		private Collider2D _collider;
 
 		private void Start()
 		{
 			_camera = Camera.main;
-			_collider = GetComponent<Collider2D>();
 		}
 
 		private void Update()
 		{
-			Vector3 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-			if (Input.GetMouseButtonDown(0)
-			    && _collider == Physics2D.OverlapPoint(mousePosition))
+			if (Input.GetMouseButtonDown(0))
 			{
 				_screenPosition = _camera.WorldToScreenPoint(transform.position);
 				Vector3 newPosition = Input.mousePosition - _screenPosition;
@@ -31,13 +26,14 @@ namespace CodeBase.Level
 				               * Mathf.Rad2Deg;
 			}
 
-			if (Input.GetMouseButton(0)
-			    && _collider == Physics2D.OverlapPoint(mousePosition))
+			if (Input.GetMouseButton(0) == false)
 			{
-				Vector3 newPosition = Input.mousePosition - _screenPosition;
-				float angle = Mathf.Atan2(newPosition.y, newPosition.x) * Mathf.Rad2Deg;
-				transform.eulerAngles = new Vector3(0, 0, angle + _angleOffset);
+				return;
 			}
- 		}
+
+			Vector3 rotate = Input.mousePosition - _screenPosition;
+			float angle = Mathf.Atan2(rotate.y, rotate.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0, 0, angle + _angleOffset);
+		}
 	}
 }
