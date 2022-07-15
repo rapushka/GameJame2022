eabb8073f10c728206d11d21fba5127b0fb5a95f
Rapuška <megarubka44@gmail.com>
2022-07-16 01:39:52 +0300

    Dadadzieny haryzantalny ruh hałoŭnamu hieroju

---
+++ GameJame2022/Assets/CodeBase/Player/PlayerInputBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Player
+{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class PlayerInputBehaviour : MonoBehaviour
+	{
+		[SerializeField] private float _moveSpeed = 1f;
+		
+		private PlayerController _input;
+		private float _direction;
+
+		private float ScaledSpeed => _moveSpeed * Time.fixedDeltaTime;
+
+		private void Awake()
+		{
+			_input = new PlayerController();
+			_input.Enable();
+		}
+
+		private void Update()
+		{
+			_direction = _input.Player.Move.ReadValue<float>();
+
+			Move(_direction);
+		}
+
+		private void Move(float direction)
+		{
+			if (Mathf.Abs(direction) < Constants.Epsilon)
+			{
+				return;
+			}
+
+			Vector3 movedPosition = transform.position;
+			movedPosition.x += direction * ScaledSpeed;
+			// ReSharper disable once Unity.InefficientPropertyAccess
+			transform.position = movedPosition;
+		}
+	}
+}
