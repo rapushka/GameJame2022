514896dfa3f0016e6f8e3c328d1be84b0b71d6d1
Rapuška <megarubka44@gmail.com>
2022-07-16 05:38:57 +0300

    Adlustravańnie heroja pa kursoru

--- GameJame2022/Assets/CodeBase/Player/PlayerCursorLooker.cs
+++ GameJame2022/Assets/CodeBase/Player/PlayerCursorLooker.cs
@@ -7,6 +7,8 @@ namespace CodeBase.Player
 	[RequireComponent(typeof(Rigidbody2D))]
 	public class PlayerCursorLooker : MonoBehaviour
 	{
+		[SerializeField] private PlayerMirror _mirror;
+		
 		private Vector2 _cursorPosition;
 		private Camera _camera;
 		private Rigidbody2D _rigidbody;
@@ -26,6 +28,8 @@ namespace CodeBase.Player
 		{
 			Vector2 facingDirection = _cursorPosition - _rigidbody.position;
 			float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
+			
+			_mirror.Flip = angle is > 90 or < -90;
 			_rigidbody.MoveRotation(angle);
 		}
 	}
---
+++ GameJame2022/Assets/CodeBase/Player/PlayerMirror.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Player
+{
+	public class PlayerMirror : MonoBehaviour
+	{
+		[SerializeField] private List<SpriteRenderer> _xFlippers;
+		[SerializeField] private List<SpriteRenderer> _yFlippers;
+
+		public bool Flip
+		{
+			set
+			{
+				foreach (SpriteRenderer sprite in _xFlippers)
+				{
+					sprite.flipX = value;
+				}
+
+				foreach (SpriteRenderer sprite in _yFlippers)
+				{
+					sprite.flipY = value;
+				}
+			}
+		}
+	}
+}
