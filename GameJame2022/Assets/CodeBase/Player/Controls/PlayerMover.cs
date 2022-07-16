a8ae84d313120988c235477d9f2f78f1da5d7a0f
Rapuška <megarubka44@gmail.com>
2022-07-16 18:54:17 +0300

    Drysta-animator

--- GameJame2022/Assets/CodeBase/Player/Controls/PlayerMover.cs
+++ GameJame2022/Assets/CodeBase/Player/Controls/PlayerMover.cs
@@ -5,14 +5,16 @@ namespace CodeBase.Player.Controls
 	public class PlayerMover : MonoBehaviour
 	{
 		[SerializeField] private float _moveSpeed;
-		
+
 		private float _direction;
-		
+
+		public bool IsMoving { get; private set; }
 		private float ScaledSpeed => _moveSpeed * Time.fixedDeltaTime;
 
 		public void Move(float direction)
 		{
-			if (Mathf.Abs(direction) < Constants.Epsilon)
+			IsMoving = Mathf.Abs(direction) > Constants.Epsilon;
+			if (IsMoving == false)
 			{
 				return;
 			}
---
+++ GameJame2022/Assets/CodeBase/Player/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+﻿using CodeBase.Player.Controls;
+using UnityEngine;
+
+namespace CodeBase.Player
+{
+	public class PlayerAnimator : MonoBehaviour
+	{
+		private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+		private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+		
+		[SerializeField] private Animator _animator;
+		[SerializeField] private PlayerMover _mover;
+
+		private void Update()
+		{
+			_animator.SetBool(IsMoving, _mover);
+		}
+	}
+}
